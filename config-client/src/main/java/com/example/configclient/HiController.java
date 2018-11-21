91bb0b7507f2ed0e5130a30c6cb901c6da05b195
xuec <huihaoweishui@163.com>
2018-11-21 12:45:12 +0800

    需要解决在maven执行打包时 某些服务直接通过${}获取配置中心的报错

--- config-client/src/main/java/com/example/configclient/HiController.java
+++ config-client/src/main/java/com/example/configclient/HiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HiController {
-//    @Value("${foo}")
+    @Value("${foo}")
     String foo;
     @RequestMapping(value = "/hi")
     public String hi(){
